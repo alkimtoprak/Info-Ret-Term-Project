129857f94ad287f170f4e0d49f9db2a2558e2a1b
buseg <buse.giledereli@boun.edu.tr>
2020-06-16 13:17:34 +0300

    is_a and history adjustments added -improving score by %1

--- baseline_matching.py
+++ baseline_matching.py
@@ -1,8 +1,11 @@
 import os
 import sys
 import json
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
-stopwords = {"ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"}
+stopwords = set(stopwords.words('english'))
 
 def create_output_files(method, dataset, token_dict, obt_dict):
     output_path = "output_" + method + "_" + dataset
